594a52d6b836c25cb642e5437b2a8ed591b5cad7
ARMs-Co <aliredamoumneh@gmail.com>
2023-05-06 18:53:11 +0300

    1d Array

--- Jarvy.java
+++ Jarvy.java
@@ -1,78 +1,46 @@
 package Javy.AMCO;
-import java.util.Scanner;
+
 
 
 public class Jarvy {
       public static void main(String[] args) throws InterruptedException {
 
-                 try( Scanner input = new Scanner(System.in)){
-                              System.out.println("Please Enter  Command");
-                              String line = input.nextLine();
-                              switch(line){
-                                    case "Y":
-                                    case "y":
-                                        System.out.println("IS THAT A YES?!");
-                                        long starttTime = System.currentTimeMillis();
-                                        while(System.currentTimeMillis() - starttTime < 2000) {
-                                            System.out.print(".");
-                                            Thread.sleep(100);
-                                        }
-                                        System.out.println("\nWELL THAT'S A NO FOR ME!");
-                                        break;
-                                    case "N":
-                                    case "n":
-                                        System.out.println("No?.....:<");
-                                        long startrrTime = System.currentTimeMillis();
-                                        while(System.currentTimeMillis() - startrrTime < 2000) {
-                                            System.out.print(".");
-                                            Thread.sleep(100);
-                                        }
-                                        System.out.println("\nFUCK YOU THEN!");
-                                        break;
-                                    case "Start":
-                                    case "start":
-                                        Thread.sleep(2000);
-                                        System.out.println("Starting Operator");
-                                        long startime = System.currentTimeMillis();
-                                        while(System.currentTimeMillis() - startime < 2000) {
-                                            System.out.print(".");
-                                            Thread.sleep(100);
-                                        }
-                                        System.out.println("\nOperator Successfully Started");
-                                        break;
-                                        case "Stop":
-                                        case "stop":
-                                        Thread.sleep(2000);
-                                        System.out.println("Stopping Operator");
-                                        long startTime = System.currentTimeMillis();
-                                        while(System.currentTimeMillis() - startTime < 2000) {
-                                            System.out.print(".");
-                                            Thread.sleep(60);
-                                        }
-                                        System.out.println("\nOperator Successfully Stopped");
-                                        break;
-                                        default:
-                                        // code for invalid command
-                                       
-                                        long startrTime = System.currentTimeMillis();
-                                        while(System.currentTimeMillis() - startrTime < 1000) {
-                                            System.out.print(".");
-                                            Thread.sleep(70);
-                                        }
-                                         System.out.println("\n command not recognized");
-                                        break;
-
-                                    
-                                        
-                                }
-                                
-                                
-                 }
-                      catch( Exception e){
-                        System.out.println(e.getMessage());
-                        
-                      }                  
-
-
+                 int value =7 ; value = value+1;
+                 int[] values; 
+                 values = new int[5]; 
+               
+                 values[0] =10;
+                 values[1] = 20;
+                 values[2] = 30;
+                 values[3] = 40;
+                 values[4] = 50;
+                ;
+               for( int i = 0; i < values.length; i++ ){
+                System.out.printf("Values = %d \n" , values[i]);
+            
+               }
+                     
+                int[] vals;
+                vals = new int[5];
+                vals[0]= 10;
+                vals[1]= 20;
+                vals[2]= 30;
+                vals[3]= 40;
+                vals[4] = 50;
+                for( int s = 0; s < vals.length; s++ ){
+                    System.out.printf("Vals = %d \n" , vals[s]);
+                    
+                }
+                String[] assoc = {"Kiwi", "Bread" , "Pineapple"};
+                for(String i : assoc){
+                    System.out.printf("assoc = "+ i + "\n");
+                }   
+                String[] Txts = new String[2];
+ 
+                System.out.printf("txt n1 = %d \n", Txts[0]);
+                Txts[0] = "Apple";
+                Thread.sleep(1000);
+                System.out.printf("txt n2 = "+Txts[0]+"\n");
+                
             }
       }
